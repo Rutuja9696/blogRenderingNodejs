56d27a8f7cc4d1848085caa07f50aa6ef1faec1d
Rutuja9696 <rutuja.patil@contentstact.com>
2020-12-16 14:44:22 +0530

    added paths to app.js

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
+const blogRouter = require("./routes/blogRoutes");
+
 const { config } = require("process");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/blogs", blogRouter);
 
 app.listen(
   process.env.PORT,
