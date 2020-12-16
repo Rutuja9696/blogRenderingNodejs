1bc650307feae637e2e87257c79350a904477919
Rutuja9696 <rutuja.patil@contentstact.com>
2020-12-16 14:40:14 +0530

    exported router

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -2,3 +2,4 @@ const express = require("express");
 const { getAllBlogs, getById } = require("../controller/actions.js");
 const router = express.Router();
 router.route("/blogs").get(getAllBlogs, getById);
+module.exports = router;
