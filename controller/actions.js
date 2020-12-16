a969cee3d68882160cdde7f9829aa9f2c7a1e202
Rutuja9696 <rutuja.patil@contentstact.com>
2020-12-16 14:36:07 +0530

    added function to fetch data by id

--- controller/actions.js
+++ controller/actions.js
@@ -10,6 +10,17 @@ const fileName = path.join(__dirname, "..", "data", "data.json");
 const blogs = JSON.parse(fs.readFileSync(fileName, "utf-8"));
 //get all blogs
 const getAllBlogs = (req, res, next) => {
-  sendResponse(200, "Successful", tasks, req, res);
+  sendResponse(200, "Successful", blogs, req, res);
 };
-module.exports = { getAllBlogs };
+//get by id
+const getById = (req, res) => {
+  const blogDisplay = blogs.find((blog) => {
+    return blog.blogId == req.params.blogId;
+  });
+  if (blogDisplay) {
+    sendResponse(200, "Successful", [blogDisplay], req, res);
+  } else {
+    sendError(new AppError(404, "Not Found", "task not available"), req, res);
+  }
+};
+module.exports = { getAllBlogs, getById };
