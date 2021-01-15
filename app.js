d2dae82c0e12c3be612f6f10fe58e51c5068b6ad
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-15 15:54:26 +0530

    Added Cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config({ path: "./config.env" });
 const blogRouter = require("./routes/blogRoutes");
 const { config } = require("process");
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/blogs", blogRouter);
